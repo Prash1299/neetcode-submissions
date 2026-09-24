7ad1cf58cf3ff70e878834d056f752e6f727e606
Prashant Singh <143712862+Prash1299@users.noreply.github.com>
2026-09-25 01:35:08 +0530

    Add: integer-break - submission-3

---
+++ Data Structures & Algorithms/integer-break/submission-3.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+
+    int solve(int n, vector<int> &dp){
+        if(n == 1){
+            return 0;
+        }
+
+        if(dp[n] != -1){
+            return dp[n];
+        }
+
+        int maxi = 0;
+
+        for(int i = 1; i<n; i++){
+            int x = i * max(n-i, solve(n-i, dp));
+            maxi = max(maxi, x);
+        }
+
+        return dp[n] = maxi;
+
+    }
+
+    int integerBreak(int n) {
+        vector<int> dp(n+1, -1);
+        
+        return solve(n, dp);
+    }
+};
